e45b5cea1a62fca2167ca28df9b1cdb66541cf32
maheshparichendi <maheshkumar.p217@gmail.com>
2022-10-09 20:02:21 +0530

    Added 3 ML Models

--- ml_model.py
+++ ml_model.py
@@ -1,2 +1,12 @@
 
 print("Adding all ML related code here")
+
+def model1():
+    print('Adding code for ML Model 1')
+
+def model2():
+    print('Adding code for ML Model 2')
+
+
+def model3():
+    print('Adding code for ML Model 3')
